fd90c8ae4c7e4a95b8b7c2ec9c6558ced4054d1a
tamilanr41 <tamilanr41@gmail.com>
2026-06-01 12:46:28 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const { notFound, errorHandler } = require('./middleware/errorHandler')
 
 const app = express()
+app.set('trust proxy', 1)
 const PORT = process.env.PORT || 5000
 
 // CORS
